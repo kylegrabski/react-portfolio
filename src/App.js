614ab3fe9d3abdbf960f38efcfa1744d0f70fa40
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-25 11:45:35 -0700

    add Jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Header/Navbar/Navbar'
+import Jumbotron from './components/Jumbotron/Jumbotron'
 
 function App() {
   return (
     <>
       <Router>
         <Navbar/>
+        <Jumbotron/>
       </Router>
     </>
   );
