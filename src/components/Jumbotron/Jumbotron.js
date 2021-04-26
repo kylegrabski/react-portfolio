8d7b94b4edd67d87f0f72a08bd82a5cffd3be22b
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-25 17:48:11 -0700

    change styles/add main page animations

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./jumbotron.module.css";
 import Container from "@material-ui/core/Container";
 import Fade from "react-reveal/Fade";
+import TextLoop from "react-text-loop";
 
 // @ToDo style more, have image be at top, word div needs to be centered and mobile friendly
 function Jumbotron() {
@@ -24,9 +25,18 @@ function Jumbotron() {
         /> */}
         <div className={styles.logo}>
           <div className={styles.welcomeName}>Kyle Grabski</div>
-          <Fade top cascade>
-            <div className={styles.slogan}>WEB DESIGN MADE SIMPLE</div>
-          </Fade>
+          <div className={styles.slogan}>
+            <Fade top cascade>
+              <div>WEB/</div>
+            </Fade>
+            <Fade bottom cascade>
+              <div>DESIGN/</div>
+            </Fade>
+            <Fade right cascade>
+              <div>MADE/</div>
+            </Fade>
+            <TextLoop children={[" Simple", " CREATIVELY"," FUN", " EYE CATCHING", " INTERACTIVE", " PEACEFULLY"]}></TextLoop>
+          </div>
         </div>
         <br />
       </Container>
