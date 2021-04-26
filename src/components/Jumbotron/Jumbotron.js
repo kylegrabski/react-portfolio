2373d6cccec4b37265ca35eff72444f35bfbb0ce
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-26 00:11:59 -0700

    add icons in footer

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,9 +3,12 @@ import styles from "./jumbotron.module.css";
 import Container from "@material-ui/core/Container";
 import Fade from "react-reveal/Fade";
 import TextLoop from "react-text-loop";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 // @ToDo style more, have image be at top, word div needs to be centered and mobile friendly
 function Jumbotron() {
+  const matches = useMediaQuery("(min-width:600px)");
+
   return (
     <>
       <Container
@@ -15,26 +18,28 @@ function Jumbotron() {
       >
         <div className={styles.logo}>
           <div className={styles.welcomeName}>Kyle Grabski</div>
-          <div className={styles.slogan}>
-            <Fade top cascade>
-              <div>WEB/</div>
-            </Fade>
-            <Fade bottom cascade>
-              <div>DESIGN/</div>
-            </Fade>
-            <Fade right cascade>
-              <div>MADE/</div>
-            </Fade>
-            <TextLoop
-              children={[
-                " SIMPLE",
-                " CREATIVELY",
-                " FUN",
-                " EYE CATCHING",
-                " INTERACTIVE",
-                " STRESS FREE",
-              ]}
-            ></TextLoop>
+          <div className={styles.slogan} matches={matches}>
+           
+              <Fade top cascade>
+                <div>WEB/</div>
+              </Fade>
+              <Fade bottom cascade>
+                <div>DESIGN/</div>
+              </Fade>
+              <Fade right cascade>
+                <div>MADE/</div>
+              </Fade>
+              <TextLoop
+                children={[
+                  " SIMPLE",
+                  " CREATIVELY",
+                  " FUN",
+                  " EYE CATCHING",
+                  " INTERACTIVE",
+                  " STRESS FREE",
+                ]}
+              ></TextLoop>
+
           </div>
         </div>
         <br />
