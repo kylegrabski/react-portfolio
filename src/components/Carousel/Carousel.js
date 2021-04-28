6774aabba60b597e545bacc93fc3ed18d22033ff
Kyle Grabski <kyle.grabski@gmail.com>
2021-04-27 18:48:30 -0700

    init commit

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+
+function CarouselProjects(props) {
+  console.log(props);
+  return (
+    <Carousel>
+      <Carousel.Item interval={3000}>
+        <img
+          className="d-block w-100"
+          src="/images/workout-tracker.png"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <img
+          className="d-block w-100"
+          src="/images/budget-tracker.png"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={3000}>
+        <img
+          className="d-block w-100"
+          src="/images/spoofify.png"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselProjects;
